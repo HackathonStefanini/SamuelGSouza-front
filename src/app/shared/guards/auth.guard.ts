42d41ddc27d07ad6a631fb76cce9f7b653176c2d
SamuelGSouza <samuelgomes318@gmail.com>
2023-02-26 17:53:52 -0300

    feat: implementa guard

--- src/app/shared/guards/auth.guard.ts
+++ src/app/shared/guards/auth.guard.ts
@@ -1,14 +1,22 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { LoginService } from '../services/login.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+  constructor(private loginService: LoginService, private routerService: Router){}
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+
+    if (this.loginService.estaLogado()) {
+      return true;
+    }
+    this.routerService.navigate(["/home"]);
+    return false;
   }
 }
